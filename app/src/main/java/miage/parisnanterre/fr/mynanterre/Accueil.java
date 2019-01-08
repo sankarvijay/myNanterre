836b55e1ee096c32430e489ce3f6f9792562bbd9
Sankar Vijay <vijay93700@gmail.com>
2019-01-08 11:10:45 +0100

    gradle configuration v2

---
+++ app/src/main/java/miage/parisnanterre/fr/mynanterre/Accueil.java
@@ -0,0 +1,13 @@
+package miage.parisnanterre.fr.mynanterre;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.accueil);
+    }
+}
