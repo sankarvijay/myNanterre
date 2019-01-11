c251bdb653565aff7614b31e1f341d4b43ae987c
Sankar Vijay <vijay93700@gmail.com>
2019-01-11 15:48:52 +0100

    liste offres version 0.1

--- app/src/main/java/miage/parisnanterre/fr/mynanterre/Accueil.java
+++ app/src/main/java/miage/parisnanterre/fr/mynanterre/Accueil.java
@@ -1,13 +1,69 @@
 package miage.parisnanterre.fr.mynanterre;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Accueil extends AppCompatActivity {
+    private List<Offre> listeOffre = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private OffreAdapter oAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        oAdapter = new OffreAdapter(listeOffre);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(oAdapter);
+        prepareOffreData();
+    }
+
+    private void prepareOffreData() {
+        Offre offre = new Offre(R.drawable.sopra,"Développeur Web H/F","Stage","Paris","\n" +
+                "Dans le cadre de son évolution et pour répondre à des besoins techniques la société recherche un développeur ..."
+        ,"10/01/2019","Sopra Steria");
+        listeOffre.add(offre);
+
+        offre = new Offre(R.drawable.total,"Ingénieur d'études et développement Java J2EE H/F","CDI","Paris La Défense","\n" +
+                "Dans le cadre du développement des activités de la branche Gas & Power, la société recherche un ...\n"
+                ,"05/01/2019","Total");
+        listeOffre.add(offre);
+
+        offre = new Offre(R.drawable.sopra,"Ingénieur d'études et développement .Net H/F","Stage","Paris","\n" +
+                "Dans le cadre du développement des activités de la branche Gas & Power, la société recherche ...\n"
+                ,"08/01/2019","Sopra Steria");
+        listeOffre.add(offre);
+
+        offre = new Offre(R.drawable.thales,"Développeur J2EE H/F","Stage","Genevilliers","\n" +
+                "Pour réaliser une nouvelle solution, Thalès recherche de nombreux stagiaires dans le ...\n"
+                ,"06/01/2019","Thalès");
+        listeOffre.add(offre);
+
+        offre = new Offre(R.drawable.dassault,"Refonte d'un outil de gestion H/F","Stage","Paris","\n" +
+                "Dans le cadre du projet de refonte de l'outil de gestion des demandes d'accès au système d'information, il est envisagé de mettre en place une gestion ...\n"
+                ,"01/01/2019","Dassault Aviation");
+        listeOffre.add(offre);
+
+        oAdapter.notifyDataSetChanged();
     }
 }
+
---
+++ app/src/main/java/miage/parisnanterre/fr/mynanterre/OffreAdapter.java
@@ -0,0 +1,62 @@
+package miage.parisnanterre.fr.mynanterre;
+
+/**
+ * Created by Sankar Vijay on 11/01/2019.
+ */
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class OffreAdapter extends RecyclerView.Adapter<OffreAdapter.MyViewHolder> {
+
+    private List<Offre> listeOffres;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView titre, entreprise, datePublication,localisation,descriptif;
+        private ImageView logo;
+
+        public MyViewHolder(View view) {
+            super(view);
+            titre = (TextView) view.findViewById(R.id.titre);
+            entreprise = (TextView) view.findViewById(R.id.entreprise);
+            datePublication = (TextView) view.findViewById(R.id.datePublication);
+            localisation = (TextView) view.findViewById(R.id.localisation);
+            descriptif = (TextView) view.findViewById(R.id.descriptif);
+            logo = (ImageView) view.findViewById(R.id.logo);
+        }
+    }
+
+
+    public OffreAdapter(List<Offre> listeOffres) {
+        this.listeOffres = listeOffres;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.liste_offres, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Offre offre = listeOffres.get(position);
+        holder.titre.setText(offre.getTitre());
+        holder.entreprise.setText(offre.getEntreprise());
+        holder.datePublication.setText(offre.getDatePublication());
+        holder.localisation.setText(offre.getLocalisation());
+        holder.descriptif.setText(offre.getDescriptif());
+        holder.logo.setImageResource(offre.getLogo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listeOffres.size();
+    }
+}
