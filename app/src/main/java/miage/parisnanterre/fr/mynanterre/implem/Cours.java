1521c4f8170571696eccb014b998b5eb8ee8f74a
marievllt <marievouillot@hotmail.fr>
2019-02-03 20:25:58 +0100

    inscription seance fait

---
+++ app/src/main/java/miage/parisnanterre/fr/mynanterre/implem/Cours.java
@@ -0,0 +1,52 @@
+package miage.parisnanterre.fr.mynanterre.implem;
+
+public class Cours {
+    private String titre,description,matiere,niveau;
+
+    public Cours() {
+    }
+
+    //constructeur
+    public Cours(String titre, String description, String matiere, String niveau) {
+        this.titre = titre;
+        this.description = description;
+        this.matiere = matiere;
+        this.niveau=niveau;
+
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMatiere() {
+        return matiere;
+    }
+
+    public void setMatiere(String matiere) {
+        this.matiere = matiere;
+    }
+
+    public String getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(String niveau) {
+        this.niveau = niveau;
+    }
+
+}
+
+
