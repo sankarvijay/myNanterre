4f885c1048c2d03ec546bd6fa0ed546e657b2829
dorianvieira <vieira.dorian@outlook.fr>
2019-03-02 22:22:29 +0100

    refactor(accueil):ajout des CGU

--- app/src/main/java/miage/parisnanterre/fr/mynanterre/fragment/AccueilFragment.java
+++ app/src/main/java/miage/parisnanterre/fr/mynanterre/fragment/AccueilFragment.java
@@ -1,5 +1,6 @@
 package miage.parisnanterre.fr.mynanterre.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
 import miage.parisnanterre.fr.mynanterre.R;
+import miage.parisnanterre.fr.mynanterre.implem.Accueil;
+import miage.parisnanterre.fr.mynanterre.implem.Cgu;
+import miage.parisnanterre.fr.mynanterre.implem.ListeCrous;
+import miage.parisnanterre.fr.mynanterre.implem.ListeSport;
+import miage.parisnanterre.fr.mynanterre.implem.TrainLigneL;
+import miage.parisnanterre.fr.mynanterre.implem.TrainRerA;
 
 /**
  * Created by Sankar Vijay on 18/01/2019.
@@ -23,7 +32,19 @@ public class AccueilFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.accueil, container, false);
+        View v =inflater.inflate(R.layout.accueil, container, false);
+
+        Button mButton = (Button) v.findViewById(R.id.cgu);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create intent
+                Intent intent = new Intent(v.getContext(), Cgu.class);
+                //Start details activity
+                startActivity(intent);
+            }
+        });
+        return v;
 
     }
 
